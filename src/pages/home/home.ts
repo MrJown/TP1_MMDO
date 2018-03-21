9a1f5fc4f41f5db57982f91cbf0918e8c3b4520c
MrJown <naes.lagrange@gmail.com>
2018-03-21 14:53:26 +0100

    ModifHomeFake

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+export interface Result {
+  title: string;
+  author: string;
+  date: number;
+  //image: string;
+
+}
+
+const fakeResults: Result[] = [
+  {title: 'Test', author: 'TestA', date:1997},
+  {title: 'Test2', author: 'Test2A', date:2018},
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
